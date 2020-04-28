917053e5477d8201e7e72d2c46471322d60a8dfa
CharOConnell <charlotteoconnell@virginmedia.com>
2020-04-28 11:35:48 +0000

    Added materialize functionality into base.html. Checked that the mongoDB was connecting properly

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 import os
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template
+# redirect, url_for, request
 from flask_pymongo import PyMongo
 # from bson.objectid import ObjectId
 
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = 'lettuce_cook'
-app.config["MONGO_URI"] = os.getenv('MONGO_URI','mongodb://localhost')
+app.config["MONGO_DBNAME"] = 'lettuce-cook'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 mongo = PyMongo(app)
 
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', recipes=mongo.db.recipes.find())
+
+
+if __name__ == '__main__':
+    app.run(host=os.getenv('IP', "0.0.0.0"), port=int(
+        os.getenv('PORT', "5000")), debug=True)
