272e34eecfce802b51974ce8819c044d8780af5d
CharOConnell <charlotteoconnell@virginmedia.com>
2020-05-06 09:06:22 +0000

    Check to see if on search page ready for interactive change for search

--- static/js/filterSelect.js
+++ static/js/filterSelect.js
@@ -4,5 +4,16 @@ function filterSelect(dropdownMenu){
         var newidtag = $("#"+idtag).siblings(".dropdown-menu").attr("id");
         var newinput = $("#"+newidtag+" option:selected").html();
         $("#"+idtag).html(newinput);
-        });
+    });
+    
+    // Check to see if we are in the search window
+    windowPath = window.location.href;
+    windowPathlen = windowPath.length;
+    windowName = windowPath.slice((windowPathlen-13),(windowPathlen-7));
+
+
+    if (windowName === "search") {
+        console.log($(this).siblings());    
+    }
+    
 }
