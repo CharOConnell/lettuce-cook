ccea9cde381a13a83ef4edfb180bbc577be9c550
CharOConnell <charlotteoconnell@virginmedia.com>
2020-05-07 08:55:04 +0000

    Tidied up the javascript function and id tags in search.html

--- static/js/filterSelect.js
+++ static/js/filterSelect.js
@@ -1,30 +1,35 @@
 function filterSelect() {
-    $(".dropdown-menu option").click(function(){
-        let idtag = $(this).parents(".dropdown").find(".input-field-dropdown").attr("id");
-        let newidtag = $("#"+idtag).siblings(".dropdown-menu").attr("id");
-        let newinput = $("#"+newidtag+" option:selected").html();
-        $("#"+idtag).html(newinput);
-    });
+  $(".dropdown-menu option").click(function () {
+    let idtag = $(this).parents(".dropdown").find(".input-field-dropdown").attr("id");
+    let newidtag = $("#" + idtag).siblings(".dropdown-menu").attr("id");
+    let newinput = $("#" + newidtag + " option:selected").html();
+    $("#" + idtag).html(newinput);
+  });
 
-    // Check to see if we are in the search window
-    windowPath = window.location.href;
-    windowPathlen = windowPath.length;
-    windowName = windowPath.slice((windowPathlen-13),(windowPathlen-7));
+  // Check to see if we are in the search window
+  windowPath = window.location.href;
+  windowPathlen = windowPath.length;
+  windowName = windowPath.slice(windowPathlen - 13, windowPathlen - 7);
 
-    if (windowName === "search") {
-        $(".dropdown-menu option").click(function(){
-            let containerid = $(this).parents(".searching").attr("id");
-            let idtag = $(this).parents(".dropdown").find(".input-field-dropdown").attr("id");
-            let category = $("#"+idtag).html();
-            let newidtag = $("#"+containerid).find(".input").attr("id");
+  if (windowName === "search") {
+    addEntryBox();
+  }
+}
 
-            if (category === "Recipe Name") {
-                $("#"+newidtag).html(`<div class="input-field col">
+function addEntryBox() {
+  $(".dropdown-menu option").click(function () {
+    let containerid = $(this).parents(".searching").attr("id");
+    let idtag = $(this).parents(".dropdown").find(".input-field-dropdown").attr("id");
+    let category = $("#" + idtag).html();
+    let newidtag = $("#" + containerid).find(".input").attr("id");
+
+    if (category === "Recipe Name") {
+      $("#" + newidtag).html(`<div class="input-field col">
                     <input type="text" name="recipe_name" id="recipe_name" placeholder="Recipe name to search for" required class="entry-data"/>
                 </div>`);
-                $("#"+newidtag).addClass("entry-box");
-            } else if (category === "Cuisine Type") {
-                $("#"+newidtag).html(`<div class="col dropdown">
+      $("#" + newidtag).addClass("entry-box");
+    } else if (category === "Cuisine Type") {
+      $("#" + newidtag).html(`<div class="col dropdown">
                     <div class="input-field-dropdown dropdown-toggle updateMe" role="button" id="dropdownMenuButtonCui" data-toggle="dropdown" aria-haspopup="false" aria-expanded="false">
                     Choose the cuisine type
                     </div>
@@ -35,9 +40,9 @@ function filterSelect() {
                         {% endfor %}
                     </select>
                 </div>`);
-                $("#"+newidtag).addClass("entry-box");
-            } else if (category === "Difficulty Level") {
-                $("#"+newidtag).html(`<div class="col dropdown">
+      $("#" + newidtag).addClass("entry-box");
+    } else if (category === "Difficulty Level") {
+      $("#" + newidtag).html(`<div class="col dropdown">
                     <div class="input-field-dropdown dropdown-toggle updateMe" role="button" id="dropdownMenuButtonDiff" data-toggle="dropdown" aria-haspopup="false" aria-expanded="false">
                     Choose the difficulty level
                     </div>
@@ -48,9 +53,9 @@ function filterSelect() {
                         {% endfor %}
                     </select>
                 </div>`);
-                $("#"+newidtag).addClass("entry-box");
-            } else if (category === "Preparation Time") {
-                $("#"+newidtag).html(`<div class="col dropdown">
+      $("#" + newidtag).addClass("entry-box");
+    } else if (category === "Preparation Time") {
+      $("#" + newidtag).html(`<div class="col dropdown">
                     <div class="input-field-dropdown dropdown-toggle updateMe" role="button" id="dropdownMenuButtonPrep" data-toggle="dropdown" aria-haspopup="false" aria-expanded="false">
                     Choose the preparation time
                     </div>
@@ -61,9 +66,9 @@ function filterSelect() {
                         {% endfor %}
                     </select>
                 </div>`);
-                $("#"+newidtag).addClass("entry-box");
-            } else if (category === "Cooking Time") {
-                $("#"+newidtag).html(`<div class="col dropdown">
+      $("#" + newidtag).addClass("entry-box");
+    } else if (category === "Cooking Time") {
+      $("#" + newidtag).html(`<div class="col dropdown">
                     <div class="input-field-dropdown dropdown-toggle updateMe" role="button" id="dropdownMenuButtonCook" data-toggle="dropdown" aria-haspopup="false" aria-expanded="false">
                     Choose the cooking time
                     </div>
@@ -74,17 +79,14 @@ function filterSelect() {
                         {% endfor %}
                     </select>
                 </div>`);
-                $("#"+newidtag).addClass("entry-box");
-            } else if (category === "Serving Size") {
-                $("#"+newidtag).html(`<div class="input-field col">
+      $("#" + newidtag).addClass("entry-box");
+    } else if (category === "Serving Size") {
+      $("#" + newidtag).html(`<div class="input-field col">
                     <input type="text" name="serving_size" id="serving_size" placeholder="Serving size to search for" required class="entry-data"/>
                 </div>`);
-                $("#"+newidtag).addClass("entry-box");
-            } else {
-                $("#"+newidtag).html(``);
-            }
-            
-
-        });
+      $("#" + newidtag).addClass("entry-box");
+    } else {
+      $("#" + newidtag).html(``);
     }
+  });
 }
