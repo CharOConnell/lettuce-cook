4077ab923735433753205b4da7aca9de7a03e4dd
CharOConnell <charlotteoconnell@virginmedia.com>
2020-05-11 12:50:31 +0000

    Added delimiter to method layout. Put into results page

--- static/js/buttonUpdate.js
+++ static/js/buttonUpdate.js
@@ -2,15 +2,23 @@ function buttonUpdate(id) {
     let innerdata = $("#"+id).html();
     // get the id of the recipe being viewed
     let fullid = id.slice(3);
-    // split up the ingredients by the delimiter ","
+    // split up the ingredients by the delimiter "," and method with "\n"
     let ingredientsChange = $("#ingredients"+fullid).html().split(",");
     let ingredientsUpdate = "";
-    // add a line to the ingredients at the delimiter
+    var methodChange = $("#method"+fullid).html().split("\n");
+    methodChange = methodChange.slice(1);
+    let methodUpdate = "";
+
+    // add a line at the delimiter
     ingredientsChange.forEach(function(item, index) {
         ingredientsUpdate += item + "<br>";});
-    // update the ingredients with the new lines
+    methodChange.forEach(function(item, index) {
+        methodUpdate += item + "<br>";});
+
+    // update the html with the new lines
     $("#ingredients"+fullid).html(`${ingredientsUpdate}`);
-    
+    $("#method"+fullid).html(`${methodUpdate}`);
+
     // update the text within the buttons
     if (innerdata == "Show More +") {
         $("#"+id).html("Show Less -");
