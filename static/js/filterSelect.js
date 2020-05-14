9750552f81bef291b66c8a56b584c38144fac5b3
CharOConnell <charlotteoconnell@virginmedia.com>
2020-05-14 11:18:51 +0000

    Removed unnecessary JS file

--- static/js/filterSelect.js
+++
@@ -1,9 +0,0 @@
-function filterSelect() {
-    // Update the dropdown menu to the option selected
-    $(".dropdown-menu option").click(function () {
-        let idtag = $(this).parents(".dropdown").find(".input-field-dropdown").attr("id");
-        let newidtag = $("#" + idtag).siblings(".dropdown-menu").attr("id");
-        let newinput = $("#" + newidtag + " option:selected").html();
-        $("#" + idtag).html(newinput);
-    });
-}
