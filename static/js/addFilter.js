7f5f8a0ba8a4ce06e65820724a1357e4e162be35
CharOConnell <charlotteoconnell@virginmedia.com>
2020-05-14 11:25:16 +0000

    Updated add filter button to include new dropdown

--- static/js/addFilter.js
+++ static/js/addFilter.js
@@ -16,29 +16,26 @@ function addFilter() {
         
         }
         if ($("#"+i).html() === "") {
-            // get the special tag names
-            let id1val = "dropdownMenuButtonFilt"+i;
-            let id2val = "filter"+i;
+            let filterval = "filter"+i;
             let inputval = "input"+i;
             // if there is no filter, add the filter option boxes
             $("#"+i).html(`<div class="row entry-box">
-                        <div class="col dropdown">
-                            <div class="input-field-dropdown dropdown-toggle" role="button" id=${id1val} data-toggle="dropdown" aria-haspopup="false" aria-expanded="false">
-                            Pick the category to search in
-                            </div>
-
-                            <select id=${id2val} class="dropdown-menu" aria-labelledby=${id1val} multiple required onchange="filterSelect(this); addEntryBoxSearch();">
-                                <option value="recipe_name" class="dropdown-item">Recipe Name</option>
-                                <option value="cuisine_type" class="dropdown-item">Cuisine Type</option>
-                                <option value="difficulty_level" class="dropdown-item">Difficulty Level</option>
-                                <option value="prep" class="dropdown-item">Preparation Time</option>
-                                <option value="cook" class="dropdown-item">Cooking Time</option>
-                                <option value="serving_size" class="dropdown-item">Serving Size</option>
-                            </select>
-                        </div>
+                <!-- FILTER DROPDOWN -->
+                    <div class="col">
+                        <select class="form-control" name="${filterval}" id="${filterval}" onchange="addBoxSearch(value, id);">
+                            <option value="">Search Category</option>
+                            <option value="recipe_name">Recipe Name</option>
+                            <option value="cuisine_type">Cuisine Type</option>
+                            <option value="difficulty_level">Difficulty Level</option>
+                            <option value="prep">Preparation Time</option>
+                            <option value="cook">Cooking Time</option>
+                            <option value="serving_size">Serving Size</option>
+                        </select>
                     </div>
-                    
-                    <div class="row input" id=${inputval}></div>`);
+                </div>
+
+                <!-- INPUT FIELD - Linked to the choice in the dropdown above -->
+                <div class="row input" id="${inputval}"></div>`);
             // break so it produces one extra filter at a time, not 6 at once
             break
         }        
